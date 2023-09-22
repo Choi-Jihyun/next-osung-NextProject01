3533553acc813b2a060890510285b8dcaf8c3ece
osung_jihyun <61964114+Choi-Jihyun@users.noreply.github.com>
2023-09-22 20:46:47 +0900

    text api

--- components/projects/noteItem.js
+++ components/projects/noteItem.js
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function NoteItem({note}) {
 
-  const noteTitle = note.properties["설명"].rich_text[0].plain_text || "제목 없음"
+  const noteTitle = note.properties["이름"].title[0]?.text.content || "제목 없음"
+  const noteText = note.properties["설명"].rich_text[0].plain_text || "내용 없음"
   const noteImg = note.cover.file?.url || note.cover.external.url
   console.log('noteTitle: ', noteTitle);
   console.log('noteImg: ', noteImg);
@@ -11,7 +12,7 @@ export default function NoteItem({note}) {
     <>
       <div className="p-6 bg-white drop-shadow-md rounded-md">
         <Image
-          className="rounded-t-xl"
+          className="rounded-t-xl w-full h-50"
           src={noteImg}
           alt="cover image"
           width={500}
@@ -20,7 +21,8 @@ export default function NoteItem({note}) {
           objectFit="cover"
           quality={100}
         />
-        <h1 className="text-black">{noteTitle}</h1>
+        <h1 className="text-black text-xl pt-2">{noteTitle}</h1>
+        <h2 className="text-black text-base pt-1">{noteText}</h2>
       </div>
     </>
   )
