d090acd6ce51c77a5766443a3a2b74f806626f2a
osung_jihyun <61964114+Choi-Jihyun@users.noreply.github.com>
2023-09-22 14:57:39 +0900

    button link

--- components/home/myProjectsSection.js
+++ components/home/myProjectsSection.js
@@ -17,7 +17,7 @@ export default function MyProjectsSection() {
             </p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                프로젝트 보러가기
+                <Link href="/project">프로젝트 보러가기</Link>
               </button>
             </div>
           </div>
