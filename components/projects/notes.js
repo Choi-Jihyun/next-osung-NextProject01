d6de0248339f2ae9e6836c6d9d5ba734c204e96a
osung_jihyun <61964114+Choi-Jihyun@users.noreply.github.com>
2023-09-24 02:32:15 +0900

    button border

--- components/projects/notes.js
+++ components/projects/notes.js
@@ -5,37 +5,24 @@ import NoteItem from "@/components/projects/noteItem";
 
 export default function Notes({ noteData }) {
 
-  const animalSortFilter = [
-    { name: '모두', filter: '', bgcolor: '#fff'},
-    { name: '포유류', filter: '포유류', bgcolor: 'bg-[#ff9f3f99]' },
-    { name: '파충류', filter: '파충류', bgcolor: '#D2B48C99' },
-    { name: '양서류', filter: '양서류', bgcolor: '#A8FF9E99' },
-    { name: '조류', filter: '조류', bgcolor: '#ff8bc599' },
-    { name: '어류', filter: '어류', bgcolor: '#82cfff99' },
+  const animalSortButton = [
+    { name: '모두', bgcolor: 'bg-slate-600'},
+    { name: '포유류', bgcolor: 'bg-[#ff9f3f99]' },
+    { name: '파충류', bgcolor: 'bg-[#D2B48C99]' },
+    { name: '양서류', bgcolor: 'bg-[#A8FF9E99]' },
+    { name: '조류', bgcolor: 'bg-[#ff8bc599]' },
+    { name: '어류', bgcolor: 'bg-[#82cfff99]' },
   ]
   const [selectedTag, setSelectedTag] = useState("모두");
-
 
 
   return (
     <div>
       <div className="flex space-x-3 flex-row items-center justify-center pt-5">
-        {animalSortFilter.map((button, index) => (
+        {animalSortButton.map((button, index) => (
           <button
             key={index}
-            className={`px-3 py-1 rounded-md ${
-              button.name === '어류'
-                ? 'bg-[#82cfff99]'
-                : button.name === '양서류'
-                ? 'bg-[#A8FF9E99]'
-                : button.name === '파충류'
-                ? 'bg-[#D2B48C99]'
-                : button.name === '조류'
-                ? 'bg-[#ff8bc599]'
-                : button.name === '포유류'
-                ? 'bg-[#ff9f3f99]'
-                : 'bg-slate-600  border-2 border-gray-300'
-            }`}
+            className={`px-3 py-1 rounded-md ${button.bgcolor} ${selectedTag === button.name ? " border border-white" : ""}`}
             onClick={() => setSelectedTag(button.name)}
           >
             {button.name}
